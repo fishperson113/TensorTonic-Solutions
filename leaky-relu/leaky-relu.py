862b5703bef363421740a73918605e0871cd2075
Phạm Triều Dương <23520355@gm.uit.edu.vn>
2026-05-08 09:58:22 +0700

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,6 +5,6 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    u= np.asarray(x)
+    u= np.array(x)
     
     return np.where(u<0,alpha*u,u)
